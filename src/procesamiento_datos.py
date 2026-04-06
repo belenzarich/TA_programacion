8af4b3534fa1835ab7b528201046f42086120fe0
belenzarich <bzarich@udesa.edu.ar>
2026-04-06 10:27:08 -0300

    Create procesamiento_datos.py

---
+++ src/procesamiento_datos.py
@@ -0,0 +1,24 @@
+def filtrar_por_pacientes(lista, id_buscado):
+    '''
+    filtra una lista de diccionarios con un id buscado y devuelve los datos de ese id.
+
+    Parameters
+    ----------
+    lista : list
+        una lista que contiene diccionarios con datos de los pacientes.
+    id_buscado : int
+        numero de id del paciente cuyos datos se quieren obtener.
+
+    Returns
+    -------
+    filtrados : list
+        lista con los datos del paciente buscado.
+
+    '''
+    filtrados = []
+    
+    for dato in lista:
+        if dato["id_participante"] == id_buscado:
+            filtrados.append(dato)
+    
+    return filtrados
