93c2dfd6b0d85b45cd857d9d7de8c1c3878c408e
belenzarich <bzarich@udesa.edu.ar>
2026-04-15 09:45:02 -0300

    Update main.py

--- main.py
+++ main.py
@@ -14,31 +14,33 @@ else:
         id_participante = int(id_participante_str)
     except ValueError:
         print('El ID debe ser un numero')
-
-    datos_part = filtrar_por_participante(datos, id_participante)
     
-    if len(datos_part) == 0:
-        print('No se encontraron datos para ese participante')
-        
     else:
-        try:
-           promedio = calcular_senal_promedio(datos_part)
-        except ValueError as e:
-            print("Error:", e)
-        try:
-            maximo = calcular_maximo_senal(datos_part)
-        except ValueError as e:
-            print("Error:", e)
-        try:
-            minimo = calcular_minimo_senal(datos_part)
-        except ValueError as e:
-            print("Error:", e)
-        try:
-            frecuencia = calcular_fc_desde_datos(datos_part)
-        except ValueError as e:
-            print("Error: ", e)
 
-        print("Promedio:", promedio)
-        print("Máximo:", maximo)
-        print("Mínimo:", minimo)
-        print("Frecuencia cardíaca:", frecuencia)
+        datos_part = filtrar_por_participante(datos, id_participante)
+        
+        if len(datos_part) == 0:
+            print('No se encontraron datos para ese participante')
+            
+        else:
+            try:
+               promedio = calcular_senal_promedio(datos_part)
+            except ValueError as e:
+                print("Error:", e)
+            try:
+                maximo = calcular_maximo_senal(datos_part)
+            except ValueError as e:
+                print("Error:", e)
+            try:
+                minimo = calcular_minimo_senal(datos_part)
+            except ValueError as e:
+                print("Error:", e)
+            try:
+                frecuencia = calcular_fc_desde_datos(datos_part)
+            except ValueError as e:
+                print("Error: ", e)
+    
+            print("Promedio:", promedio)
+            print("Máximo:", maximo)
+            print("Mínimo:", minimo)
+            print("Frecuencia cardíaca:", frecuencia)
