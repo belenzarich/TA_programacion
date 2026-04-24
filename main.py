9e91c1f774f69c5e76f72f1722785e5d621daf4b
belenzarich <bzarich@udesa.edu.ar>
2026-04-24 12:20:00 -0300

    manejo de errores

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from src.carga_datos import cargar_datos
 from src.procesamiento_datos import filtrar_por_participante
 from src.metricas import (calcular_senal_promedio, calcular_maximo_senal, calcular_minimo_senal, calcular_fc_desde_datos)
 
-datos = cargar_datos("datos/PulseLab_mock_data_error02.csv")
+datos = cargar_datos("datos/PulseLab_mock_data.csv")
 
 if len(datos) == 0:
     print('No se pudieron cargar datos')
@@ -11,17 +11,27 @@ else:
     id_participante_str = input('ID que desea buscar: ')
     
     datos_part = filtrar_por_participante(datos, id_participante_str)
-    
-    try:
-       promedio = calcular_senal_promedio(datos_part)
-       maximo = calcular_maximo_senal(datos_part)
-       minimo = calcular_minimo_senal(datos_part)
-       frecuencia = calcular_fc_desde_datos(datos_part)
-    except ValueError as e:
-        print("Error:", e)
 
-    print("Promedio:", promedio)
-    print("Máximo:", maximo)
-    print("Mínimo:", minimo)
-    print("Frecuencia cardíaca:", frecuencia)
+    if len(datos_part) == 0:
+        print('No se encontraron datos para ese participante o el ID es inválido')
+    
+    else:
+        try:
+           promedio = calcular_senal_promedio(datos_part)
+           maximo = calcular_maximo_senal(datos_part)
+           minimo = calcular_minimo_senal(datos_part)
+           print("Promedio:", promedio)
+           print("Máximo:", maximo)
+           print("Mínimo:", minimo)
+           
+        except ValueError as e:
+            print("Error en las métricas básicas:", e)
+        
+        try:
+           frecuencia = calcular_fc_desde_datos(datos_part) 
+           print("Frecuencia cardíaca:", frecuencia)
+        except ValueError as e:
+            print("Error en la frecuencia:", e)
+
+    
 
--- src/procesamiento_datos.py
+++ src/procesamiento_datos.py
@@ -15,24 +15,20 @@ def filtrar_por_participante(lista, id_buscado):
         lista con diccionario de los datos del paciente buscado.
 
     '''
-    filtrados = []
-    if len(lista) == 0:
-        print('Lista vacía')
+    if isinstance(id_buscado, str) and id_buscado.lower() == 'todos':
+        return lista
     
     try:
         id_buscado = int(id_buscado)
-        if isinstance(id_buscado, str) and id_buscado.lower() =='todos':
-            return lista
-        print('El ID debe ser un numero o ''todos''')
-        
-    except Exception:
-        print("todo mal")
+    except ValueError:
+        print('El ID debe ser un número o "todos"')
+        return []
     
-    else:
-        for dato in lista:
-            if dato["id_participante"] == id_buscado:
-                filtrados.append(dato)  
-        if len(filtrados) == 0:
-            print('No se encontraron datos para ese participante')
-        else:
-            return filtrados
+    filtrados = []
+    
+
+    for dato in lista:
+        if dato["id_participante"] == id_buscado:
+            filtrados.append(dato)  
+
+    return filtrados
